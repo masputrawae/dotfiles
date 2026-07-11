6ac8aa25aabfeaf7fad9b5c53f18b5c63da2b925
Putra Jaya <masputrawae@users.noreply.github.com>
2026-07-11 11:53:04 +0700

    feat: add border weight

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -90,10 +90,10 @@ hl.config({
     gaps_in          = 0,
     gaps_out         = 0,
 
-    border_size      = 0,
+    border_size      = 1,
 
     col              = {
-      active_border   = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+      active_border   = { colors = { "rgba(ffffffaa)" } },
       inactive_border = "rgba(595959aa)",
     },
 
